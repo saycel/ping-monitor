a502d2e7d2ef1e345dfd4cab160b5382c6b75a91
jonmor26756 <jonathan.morales26756@gmail.com>
2016-11-16 16:50:51 -0500

    Complete overhaul of the script to keep all variables contained within the script. values are no longer echoed into files but remain variables and worked on throughout, thus shortening the script.

--- pingme.sh
+++ pingme.sh
@@ -3,82 +3,32 @@
 
 #select IP to ping
 read -p "Which IP would you like to ping? " IP
-ping -c 1 $IP > results
+results=$(ping -c 1 $IP)
 
+# Ping from IP
+field1=$(awk -F':' '{print $1}' <<< $results)
+field1=$(sed -n -e 's/^.*bytes from//p' <<< $field1)
 
-#Ping from IP
+#TIme to Live
+field2=$(awk -F'=' '{print $3}' <<< $results)
+field2=$(sed  -e 's/time//' <<< $field2)
+field2=$(awk -F':' '{print "ttl= "$0}' <<< $field2)
 
-field1 () {
-awk -F':' '{ print $1 }' results > tmp
-grep "64" tmp | awk '{ print $0"," }' > field1
-echo "Field 1 now processed."
-}
-field1
+#Packet Loss
+field3=$(awk -F',' '{print $3}' <<< $results)
 
+#Response time (in ms)
+field4=$(awk -F'=' '{print $4}' <<< $results)
+field4=$(awk -F'---' '{ print $1 }' <<< $field4)
 
-# Time to Live ttl=###
+# Hostname
+field5=$(hostname)
 
-field2 () {
-awk -F'=' '{ print $3 }' results > tmp
-sed -i 's/time//' tmp
-sed -i '/^$/d;s/[[:blank:]]//g' tmp #delete all whitespace
-awk '{ print "ttl= "$0","}' tmp > field2
-echo "Field 2 now processed."
-}
-field2
+#Date
+field6=$(date)
 
-
-# Packet loss
-
-field3 () {
-awk -F',' '{ print $3 }' results > tmp
-sed -i '/^$/d;s/[[:blank:]]//g' tmp
-awk '{ print $0"," }' tmp > field3
-echo "Field 3 now processed."
-}
-field3
-
-# Response time (in ms)
-
-field4 () {
-awk -F'=' '{ print $4 }' results > tmp
-sed -i '/^$/d;s/[[:blank:]]//g' tmp
-awk '{ print $0"," }' tmp > field4 # append , to end of line
-echo "Field 4 now processed."
-}
-field4
-
-
-
-host () {
-echo $(hostname) > tmp
-awk '{ print $0"," }' tmp > host
-echo "Hostname processed"
-}
-host
-
-datenow () {
-echo $(date) > tmp
-awk '{ print $0"," }' tmp > date
-echo "Date processed"
-}
-datenow
-
-
-postProcess () {
-rm tmp
-paste -d '\0' date host field1 field2 field3 field4 > /var/lib/mysql-files/output.csv
-rm date host field1 field2 field3 field4 results
-echo "Post processing now completed"
-}
-postProcess
-
-#Sql insertion into predefined database and table.
 source credentials
-mysql -u $user << EOF
-use $base
-LOAD DATA INFILE '/var/lib/mysql-files/output.csv'
-INTO TABLE $table
-FIELDS TERMINATED BY ',';
+mysql -u $user -p$pass $base << EOF
+INSERT INTO $table (date,hostname,IP,TTL,loss,times) VALUES 
+('$field6','$field5','$field1','$field2','$field3','$field4');
 EOF
-
